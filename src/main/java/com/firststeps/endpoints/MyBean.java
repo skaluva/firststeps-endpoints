f6680fca7317fa96a660100e9a77cad73897a906
skaluva <sreenivasulu.kaluva@ymail.com>
2014-10-15 23:29:33 +0530

    google clound endpoints-firststeps initial commit

---
+++ src/main/java/com/firststeps/endpoints/MyBean.java
@@ -0,0 +1,14 @@
+package com.firststeps.endpoints;
+/** The object model for the data we are sending through endpoints */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
